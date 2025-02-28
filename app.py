53cef41686e2a8b4578506d42e7f6ce88cc8d37a
Yash Patil <stud22.ysp1@gec.ac.in>
2025-02-28 10:46:38 +0530

    first api working

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
 
 MONGO_URI="mongodb+srv://yash:yash@cluster0.jdcnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 app=Flask(__name__)
 client=MongoClient(MONGO_URI)
-db=client('stray-db')
+db=client.get_database('stray-db')
 
 
 
-@app.route('/start'),methods=['GET'])
+@app.route('/start',methods=['GET'])
 def start():
     return "Welcome to Stray-connect"
 
